4f389d2c50df1ed5c3a8cbbd631fd8597ec7f0f4
moruch4nn <moruch4nn@gmail.com>
2023-06-28 22:43:09 +0900

    ⭐ItemStackSerializerの保存形式を変更

--- src/main/kotlin/dev/mr3n/paperallinone/configuration/kotlinx/serialization/serializer/ItemStackSerializer.kt
+++ src/main/kotlin/dev/mr3n/paperallinone/configuration/kotlinx/serialization/serializer/ItemStackSerializer.kt
@@ -3,23 +3,26 @@ package dev.mr3n.paperallinone.configuration.kotlinx.serialization.serializer
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializer
-import kotlinx.serialization.builtins.ByteArraySerializer
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 import org.bukkit.inventory.ItemStack
+import java.util.*
 
 @Serializer(forClass = ItemStack::class)
 @OptIn(ExperimentalSerializationApi::class)
 object ItemStackSerializer: KSerializer<ItemStack> {
-    private val serializer = ByteArraySerializer()
-    override val descriptor: SerialDescriptor = SerialDescriptor("ItemStack", serializer.descriptor)
+    private val encoder = Base64.getEncoder()
+    private val decoder = Base64.getDecoder()
+    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("ItemStack", PrimitiveKind.STRING)
 
     override fun serialize(encoder: Encoder, value: ItemStack) {
-        encoder.encodeSerializableValue(serializer, value.serializeAsBytes())
+        encoder.encodeString(this.encoder.encodeToString(value.serializeAsBytes()))
     }
 
     override fun deserialize(decoder: Decoder): ItemStack {
-        return ItemStack.deserializeBytes(decoder.decodeSerializableValue(serializer))
+        return ItemStack.deserializeBytes(this.decoder.decode(decoder.decodeString()))
     }
 }
