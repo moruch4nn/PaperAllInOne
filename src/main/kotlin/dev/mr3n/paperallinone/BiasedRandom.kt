da980b68181451c01df3a0a1f9b14632d554e153
moruch4nn <moruch4nn@gmail.com>
2023-06-30 20:25:37 +0900

    🐛BiasedRandomを修正

--- src/main/kotlin/dev/mr3n/paperallinone/BiasedRandom.kt
+++ src/main/kotlin/dev/mr3n/paperallinone/BiasedRandom.kt
@@ -2,15 +2,15 @@ package dev.mr3n.paperallinone
 
 import kotlin.random.Random
 
-fun <T> Map<Int, T>.biasedRandom(random: Random): T {
-    val sum = this.keys.sum()
+fun <T> Map<T, Int>.biasedRandom(random: Random): T {
+    val sum = this.values.sum()
     val v = random.nextInt(1,sum)
     var count = 0
-    this.forEach { (bias, value) ->
+    this.forEach { (value, bias) ->
         if(v in count..count+bias) {
             return value
         }
         count+=bias
     }
-    return this.values.random(random)
+    return this.keys.random(random)
 }
