e492a55b5188c3aad7aead6a50fdfc07ac29a58d
moruch4nn <moruch4nn@gmail.com>
2023-07-20 18:45:24 +0900

    ➕DisplayEntityInfoを追加

--- src/main/kotlin/dev/mr3n/paperallinone/objects/RelativeObject.kt
+++ src/main/kotlin/dev/mr3n/paperallinone/objects/RelativeObject.kt
@@ -2,6 +2,7 @@ package dev.mr3n.paperallinone.objects
 
 import org.bukkit.Location
 import org.bukkit.entity.Display
+import org.bukkit.entity.Entity
 import org.bukkit.util.Vector
 
 @Suppress("unused")
@@ -9,18 +10,18 @@ class RelativeObject(location: Location) {
 
     var location = location.clone().setDirection(Vector(0,0,0))
 
-    private val displayEntities = mutableMapOf<Display, DisplayEntityInfo>()
+    private val displayEntities = mutableMapOf<Entity, DisplayEntityInfo>()
     private val rotation = Vector()
 
-    fun add(entity: Display, vector: Vector, rotateX: Boolean = true, rotateY: Boolean = true, rotateZ: Boolean = true): RelativeObject {
+    fun add(entity: Entity, vector: Vector, rotateX: Boolean = true, rotateY: Boolean = true, rotateZ: Boolean = true): RelativeObject {
         this.displayEntities[entity] = DisplayEntityInfo(vector, rotateX, rotateY, rotateZ)
         entity.boundingBox
         return this
     }
 
-    fun add(entity: Display, x: Double, y: Double, z: Double, rotateX: Boolean = true, rotateY: Boolean = true, rotateZ: Boolean = true): RelativeObject = this.add(entity, Vector(x,y,z), rotateX, rotateY, rotateZ)
+    fun add(entity: Entity, x: Double, y: Double, z: Double, rotateX: Boolean = true, rotateY: Boolean = true, rotateZ: Boolean = true): RelativeObject = this.add(entity, Vector(x,y,z), rotateX, rotateY, rotateZ)
 
-    fun <T: Display> add(type: Class<T>, x: Double, y: Double, z: Double, rotateX: Boolean = true, rotateY: Boolean = true, rotateZ: Boolean = true): T {
+    fun <T: Entity> add(type: Class<T>, x: Double, y: Double, z: Double, rotateX: Boolean = true, rotateY: Boolean = true, rotateZ: Boolean = true): T {
         val world = this.location.world!!
         val entity = world.spawn(this.location,type)
         this.add(entity, x, y, z, rotateX, rotateY, rotateZ)
