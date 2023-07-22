0635ccc0e3126f2eba95b36a4061e3341fce71a6
moruch4nn <moruch4nn@gmail.com>
2023-07-22 18:06:12 +0900

    ➕lineToを追加

--- src/main/kotlin/dev/mr3n/paperallinone/location/LocationUtils.kt
+++ src/main/kotlin/dev/mr3n/paperallinone/location/LocationUtils.kt
@@ -9,3 +9,15 @@ fun Location.lookAt(target: Location): Location {
 fun Location.moveFront(length: Double): Location {
     return this.add(this.direction.normalize().multiply(length))
 }
+
+fun Location.lineTo(to: Location, roughness: Int, task: (Location)->Unit) {
+    val from = this.clone()
+    val now = from.clone()
+    val direction = to.toVector().subtract(from.toVector()).normalize().multiply(roughness)
+    val distance = to.distance(from)
+    while(true) {
+        val loc = now.add(direction)
+        if(from.distance(now) >= distance) { break }
+        task.invoke(loc)
+    }
+}
