aae7720ba33621ff6a8a88827dba9ba4f8cff4b2
moruch4nn <moruch4nn@gmail.com>
2023-08-06 06:53:26 +0900

    🐛バグを修正

--- src/main/kotlin/dev/mr3n/paperallinone/item/NBTContainer.kt
+++ src/main/kotlin/dev/mr3n/paperallinone/item/NBTContainer.kt
@@ -3,8 +3,10 @@
 package dev.mr3n.paperallinone.item
 
 import org.bukkit.NamespacedKey
+import org.bukkit.entity.Entity
 import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.meta.ItemMeta
+import org.bukkit.persistence.PersistentDataHolder
 import org.bukkit.persistence.PersistentDataType
 import java.util.*
 
@@ -18,133 +20,133 @@ internal object KeyManager {
     }
 }
 
-class NBTContainer(private val itemMeta: ItemMeta) {
+class NBTContainer(private val holder: PersistentDataHolder) {
     fun longArray(key: NamespacedKey, value: LongArray): NBTContainer {
-        itemMeta.persistentDataContainer.set(key, PersistentDataType.LONG_ARRAY, value)
+        holder.persistentDataContainer.set(key, PersistentDataType.LONG_ARRAY, value)
         return this
     }
 
     fun longArray(key: Enum<*>, value: LongArray) = this.longArray(KeyManager.key(key), value)
 
     fun longArray(key: NamespacedKey): LongArray? {
-        return itemMeta.persistentDataContainer.get(key, PersistentDataType.LONG_ARRAY)
+        return holder.persistentDataContainer.get(key, PersistentDataType.LONG_ARRAY)
     }
 
     fun longArray(key: Enum<*>) = this.longArray(KeyManager.key(key))
 
     fun integerArray(key: NamespacedKey, value: IntArray): NBTContainer {
-        itemMeta.persistentDataContainer.set(key, PersistentDataType.INTEGER_ARRAY, value)
+        holder.persistentDataContainer.set(key, PersistentDataType.INTEGER_ARRAY, value)
         return this
     }
 
     fun integerArray(key: Enum<*>, value: IntArray) = this.integerArray(KeyManager.key(key), value)
 
     fun integerArray(key: NamespacedKey): IntArray? {
-        return itemMeta.persistentDataContainer.get(key, PersistentDataType.INTEGER_ARRAY)
+        return holder.persistentDataContainer.get(key, PersistentDataType.INTEGER_ARRAY)
     }
 
     fun integerArray(key: Enum<*>): IntArray? = this.integerArray(KeyManager.key(key))
 
     fun byteArray(key: NamespacedKey, value: ByteArray): NBTContainer {
-        itemMeta.persistentDataContainer.set(key, PersistentDataType.BYTE_ARRAY, value)
+        holder.persistentDataContainer.set(key, PersistentDataType.BYTE_ARRAY, value)
         return this
     }
 
     fun byteArray(key: Enum<*>, value: ByteArray) = this.byteArray(KeyManager.key(key), value)
 
     fun byteArray(key: NamespacedKey): ByteArray? {
-        return itemMeta.persistentDataContainer.get(key, PersistentDataType.BYTE_ARRAY)
+        return holder.persistentDataContainer.get(key, PersistentDataType.BYTE_ARRAY)
     }
 
     fun byteArray(key: Enum<*>) = this.byteArray(KeyManager.key(key))
 
     fun double(key: NamespacedKey, value: Double): NBTContainer {
-        itemMeta.persistentDataContainer.set(key, PersistentDataType.DOUBLE, value)
+        holder.persistentDataContainer.set(key, PersistentDataType.DOUBLE, value)
         return this
     }
 
     fun double(key: Enum<*>, value: Double) = this.double(KeyManager.key(key), value)
 
     fun double(key: NamespacedKey): Double? {
-        return itemMeta.persistentDataContainer.get(key, PersistentDataType.DOUBLE)
+        return holder.persistentDataContainer.get(key, PersistentDataType.DOUBLE)
     }
 
     fun double(key: Enum<*>) = this.double(KeyManager.key(key))
 
     fun float(key: NamespacedKey, value: Float): NBTContainer {
-        itemMeta.persistentDataContainer.set(key, PersistentDataType.FLOAT, value)
+        holder.persistentDataContainer.set(key, PersistentDataType.FLOAT, value)
         return this
     }
 
     fun float(key: Enum<*>, value: Float) = this.float(KeyManager.key(key), value)
 
     fun float(key: NamespacedKey): Float? {
-        return itemMeta.persistentDataContainer.get(key, PersistentDataType.FLOAT)
+        return holder.persistentDataContainer.get(key, PersistentDataType.FLOAT)
     }
 
     fun float(enum: Enum<*>) = this.float(KeyManager.key(enum))
 
     fun long(key: NamespacedKey, value: Long): NBTContainer {
-        itemMeta.persistentDataContainer.set(key, PersistentDataType.LONG, value)
+        holder.persistentDataContainer.set(key, PersistentDataType.LONG, value)
         return this
     }
 
     fun long(key: Enum<*>, value: Long) = this.long(KeyManager.key(key), value)
 
     fun long(key: NamespacedKey): Long? {
-        return itemMeta.persistentDataContainer.get(key, PersistentDataType.LONG)
+        return holder.persistentDataContainer.get(key, PersistentDataType.LONG)
     }
 
     fun long(key: Enum<*>) = this.long(KeyManager.key(key))
 
     fun integer(key: NamespacedKey, value: Int): NBTContainer {
-        itemMeta.persistentDataContainer.set(key, PersistentDataType.INTEGER, value)
+        holder.persistentDataContainer.set(key, PersistentDataType.INTEGER, value)
         return this
     }
 
     fun integer(key: Enum<*>, value: Int) = this.integer(KeyManager.key(key), value)
 
     fun integer(key: NamespacedKey): Int? {
-        return itemMeta.persistentDataContainer.get(key, PersistentDataType.INTEGER)
+        return holder.persistentDataContainer.get(key, PersistentDataType.INTEGER)
     }
 
     fun integer(key: Enum<*>) = this.integer(KeyManager.key(key))
 
     fun short(key: NamespacedKey, value: Short): NBTContainer {
-        itemMeta.persistentDataContainer.set(key, PersistentDataType.SHORT, value)
+        holder.persistentDataContainer.set(key, PersistentDataType.SHORT, value)
         return this
     }
 
     fun short(key: Enum<*>, value: Short) = this.short(KeyManager.key(key), value)
 
     fun short(key: NamespacedKey): Short? {
-        return itemMeta.persistentDataContainer.get(key, PersistentDataType.SHORT)
+        return holder.persistentDataContainer.get(key, PersistentDataType.SHORT)
     }
 
     fun short(key: Enum<*>) = this.short(KeyManager.key(key))
 
     fun byte(key: NamespacedKey, value: Byte): NBTContainer {
-        itemMeta.persistentDataContainer.set(key, PersistentDataType.BYTE, value)
+        holder.persistentDataContainer.set(key, PersistentDataType.BYTE, value)
         return this
     }
 
     fun byte(key: Enum<*>, value: Byte) = this.byte(KeyManager.key(key), value)
 
     fun byte(key: NamespacedKey): Byte? {
-        return itemMeta.persistentDataContainer.get(key, PersistentDataType.BYTE)
+        return holder.persistentDataContainer.get(key, PersistentDataType.BYTE)
     }
 
     fun byte(key: Enum<*>) = this.byte(KeyManager.key(key))
 
     fun string(key: NamespacedKey, value: String): NBTContainer {
-        itemMeta.persistentDataContainer.set(key, PersistentDataType.STRING, value)
+        holder.persistentDataContainer.set(key, PersistentDataType.STRING, value)
         return this
     }
 
     fun string(enum: Enum<*>, value: String) = this.string(KeyManager.key(enum), value)
 
     fun string(key: NamespacedKey): String? {
-        return itemMeta.persistentDataContainer.get(key, PersistentDataType.STRING)
+        return holder.persistentDataContainer.get(key, PersistentDataType.STRING)
     }
 
     fun string(enum: Enum<*>) = this.string(KeyManager.key(enum))
@@ -162,20 +164,20 @@ class NBTContainer(private val itemMeta: ItemMeta) {
     fun uniqueId(key: Enum<*>) = this.uniqueId(KeyManager.key(key))
 
     fun boolean(key: NamespacedKey, value: Boolean): NBTContainer {
-        itemMeta.persistentDataContainer.set(key, PersistentDataType.BOOLEAN, value)
+        holder.persistentDataContainer.set(key, PersistentDataType.BOOLEAN, value)
         return this
     }
 
     fun boolean(key: Enum<*>, value: Boolean) = this.boolean(KeyManager.key(key), value)
 
     fun boolean(key: NamespacedKey): Boolean? {
-        return itemMeta.persistentDataContainer.get(key, PersistentDataType.BOOLEAN)
+        return holder.persistentDataContainer.get(key, PersistentDataType.BOOLEAN)
     }
 
     fun boolean(key: Enum<*>) = this.boolean(KeyManager.key(key))
 
     fun remove(key: NamespacedKey): NBTContainer {
-        itemMeta.persistentDataContainer.remove(key)
+        holder.persistentDataContainer.remove(key)
         return this
     }
 
@@ -184,9 +186,15 @@ class NBTContainer(private val itemMeta: ItemMeta) {
 
 fun ItemMeta.nbt(): NBTContainer = NBTContainer(this)
 
+fun Entity.nbt(): NBTContainer = NBTContainer(this)
+
 fun <T> ItemStack.nbt(block: NBTContainer.()->T): T {
     val itemMeta = this.itemMeta
     val value = block.invoke(itemMeta.nbt())
     this.itemMeta = itemMeta
     return value
+}
+
+fun <T> Entity.nbt(block: NBTContainer.() -> T): T {
+    return block.invoke(NBTContainer(this))
 }
