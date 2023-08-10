a67f78b91b8507248e2c97ce3dabc1d9e91a0c38
moruch4nn <moruch4nn@gmail.com>
2023-08-11 07:29:27 +0900

    🐛Worldが存在しない場合はnullになるように

--- src/main/kotlin/dev/mr3n/paperallinone/configuration/kotlinx/serialization/serializer/WorldSerializer.kt
+++ src/main/kotlin/dev/mr3n/paperallinone/configuration/kotlinx/serialization/serializer/WorldSerializer.kt
@@ -2,7 +2,7 @@ package dev.mr3n.paperallinone.configuration.kotlinx.serialization.serializer
 
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.KSerializer
-import kotlinx.serialization.Serializer
+import kotlinx.serialization.builtins.serializer
 import kotlinx.serialization.descriptors.PrimitiveKind
 import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.descriptors.SerialDescriptor
@@ -12,15 +12,14 @@ import org.bukkit.Bukkit
 import org.bukkit.World
 
 @OptIn(ExperimentalSerializationApi::class)
-@Serializer(forClass = World::class)
-object WorldSerializer: KSerializer<World> {
+object WorldSerializer: KSerializer<World?> {
     override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("World", PrimitiveKind.STRING)
 
-    override fun serialize(encoder: Encoder, value: World) {
-        encoder.encodeString(value.name)
+    override fun serialize(encoder: Encoder, value: World?) {
+        encoder.encodeNullableSerializableValue(String.serializer(), value?.name)
     }
 
-    override fun deserialize(decoder: Decoder): World {
-        return Bukkit.getWorld(decoder.decodeString())!!
+    override fun deserialize(decoder: Decoder): World? {
+        return Bukkit.getWorld(decoder.decodeString())
     }
 }
